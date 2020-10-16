c49f19514b87c616fb84b360ecf02e9ec5866adb
Dylan <dborc610@mtroyal.ca>
2020-10-16 16:46:56 -0600

    small js edits

--- js/index.js
+++ js/index.js
@@ -7,32 +7,17 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch(url)
     .then( (resp) => resp.json() )
     .then( data => { displayContinents(data) } )
-//    .then( data => {array = alpha.util.polish(data)['data']})
     .catch(error => console.error(error)); 
-
-//    console.log(array);    
     
 });
 
-
+const gallery = []
 
 function displayContinents(continents) {
-    const list = document.querySelector('#listOfGalleries ul');
-   
-    for (let c of continents) {
-        
-        //console.log(c.GalleryName);
-       
-    const item = document.createElement('li');
-    item.textContent = c.GalleryName;
-    list.appendChild(item);
+    for(let c of continents) {
+        gallery.push(c);
     }
-   } 
-
-   function createArray(data, array) {
-    for(let d of data){
-        array = d;
-    }
+    console.log(gallery);
 }
 
    
