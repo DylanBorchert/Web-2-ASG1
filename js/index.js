5b8a4a8b3c2cb538e0895f6ebc9cac169a52afac
Dylan <dborc610@mtroyal.ca>
2020-10-18 20:05:18 -0600

    gallery Info working

--- js/index.js
+++ js/index.js
@@ -3,11 +3,10 @@ const gallery = [];
 document.addEventListener("DOMContentLoaded", function() {
     let url = 'https://www.randyconnolly.com/funwebdev/3rd/api/art/galleries.php';
 
-
     fetch(url)
     .then( (resp) => resp.json() )
     .then( data => { displayContinents(data) } )
-    .catch(error => console.error(error)); 
+    .catch(error => console.error("Fetch Error :- " + error)); 
 
 
 function displayContinents(continents) {
@@ -15,56 +14,58 @@ function displayContinents(continents) {
         gallery.push(c);
     }
     console.log(gallery);
-    const list = document.querySelector('#listOfGalleries ul');
+    const listGalleryDiv = document.querySelector('#listOfGalleries');
+    const list = document.createElement("ul")
     for(let g of gallery){
         let item = document.createElement('li');
         item.textContent = g.GalleryName;
         list.appendChild(item);
     }
-    console.log(gallery);
-    let div = document.querySelector(".containerGallery");
-    console.log(div);
-        
-        div.addEventListener("click", populate);
+    listGalleryDiv.appendChild(list);        
+    listGalleryDiv.addEventListener("click", populate);
     
 
 }
 
 
 function populate(e){
-    let nameList = e.target.querySelector('li');
-    console.log(nameList);
-    let innerDiv = document.querySelector("div #galleryInfo");
-    let ul = document.createElement('ul');
-    innerDiv.appendChild(ul);
-    for (let g of gallery){
-        if(g.GalleryName == nameList.GalleryName){
-            console.log(g);
-            addLI(g.GalleryName, ul);
-            addLI(g.GalleryNativeName, ul);
-            addLI(g.GalleryCity, ul);
-            addLI(g.GalleryAddress, ul);
-            addLI(g.GalleryCountry, ul);
+    const addLI = function addLI (content, ul){
+        let listItem = document.createElement('li');
+        listItem.textContent = content;
+        ul.appendChild(listItem);
+    }
+    if(e.target.nodeName.toLowerCase() == "li") {
+        let nameList = e.target;
+        console.log(nameList);
+        let innerDiv = document.querySelector("div #galleryInfo");
+        let ul = document.createElement('ul');
+        innerDiv.appendChild(ul);
+        for (let g of gallery){
+            if(g.GalleryName == nameList.textContent){
+                console.warn("Line 47 loop run with " + g);
+                console.log(g);
+                addLI(g.GalleryName, ul);
+                addLI(g.GalleryNativeName, ul);
+                addLI(g.GalleryCity, ul);
+                addLI(g.GalleryAddress, ul);
+                addLI(g.GalleryCountry, ul);
 
-            let listItem = document.createElement('li');
-            let link = document.createElement('a');
-            link.setAttribute('href', `g.GalleryWebSite`);
-            link.textContent = "link";
-            listItem.appendChild(link);
-            ul.appendChild(listItem);
+                let listItem = document.createElement('li');
+                let link = document.createElement('a');
+                link.setAttribute('href', `g.GalleryWebSite`);
+                link.textContent = "link";
+                listItem.appendChild(link);
+                ul.appendChild(listItem);
+            
         
-    
+            }
         }
     }
 
 
 }
 
-function addLI (content, ul){
-    let listItem = document.createElement('li');
-    listItem.textContent = content;
-    ul.appendChild(listItem);
-}
+
 
     
 // createGalleryList(gallery);
@@ -89,6 +90,6 @@ var map;
 function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 41.89474, lng: 12.4839},
-        zoom: 6
+        zoom: 1.5
     });
 }
