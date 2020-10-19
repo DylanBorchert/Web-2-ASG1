7c203c1f20ffa423476cee6dd0ba5841b308c7db
Dylan <dborc610@mtroyal.ca>
2020-10-18 23:08:44 -0600

    grid moves with gallery and more style

--- js/index.js
+++ js/index.js
@@ -8,10 +8,18 @@ document.addEventListener("DOMContentLoaded", function() {
     .then( data => { displayContinents(data) } )
     .catch(error => console.error("Fetch Error :- " + error)); 
 
-
+    let listOfGalleries = document.querySelector("#listOfGalleries");
     let mapContainer = document.querySelector("#map");
+    let galleryMap = document.querySelector("#galleryMap");
+    let galleryInfo = document.querySelector("#galleryInfo");
+    let paintings = document.querySelector("#paintings");
     console.log(mapContainer); 
     mapContainer.style.display = "none";
+    galleryMap.style.display = "none";
+    galleryInfo.style.display = "none";
+    paintings.style.display = "none";
+    listOfGalleries.style.gridColumn = "1/4";
+
 
 function displayContinents(continents) {
     for(let c of continents){
@@ -48,23 +56,25 @@ function populate(e){
         innerDiv.appendChild(ul);
 
         mapContainer.style.display = "block";
-        
+        galleryMap.style.display = "block";
+        galleryInfo.style.display = "block";
+        paintings.style.display = "block";
+        listOfGalleries.style.gridColumn = "1/2";
 
         for (let g of gallery){
             if(g.GalleryName == nameList.textContent){
                 console.warn("Line 47 loop run with " + g);
                 
                 
-                addLI(g.GalleryName, ul);
+                addLI(`Gallery Name: ${g.GalleryName}`, ul);
                 addLI(g.GalleryNativeName, ul);
-                addLI(g.GalleryCity, ul);
+                addLI(`${g.GalleryCity}, ${g.GalleryCountry}`, ul);
                 addLI(g.GalleryAddress, ul);
-                addLI(g.GalleryCountry, ul);
 
                 let listItem = document.createElement('li');
                 let link = document.createElement('a');
                 link.setAttribute('href', `${g.GalleryWebSite}`);
-                link.textContent = "link";
+                link.textContent = "Gallery Web Site";
                 listItem.appendChild(link);
                 ul.appendChild(listItem);
 
@@ -83,8 +93,92 @@ function populate(e){
 var map
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 16,
-      center: { lat: -10.363882, lng: 50.044922 }
+      zoom: 2,
+      center: { lat: 0, lng: 50 },
+      styles: [
+        {
+          elementType: "geometry",
+          stylers: [{ color: "#f5f5f5" }],
+        },
+        {
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#616161" }],
+        },
+        {
+          elementType: "labels.text.stroke",
+          stylers: [{ color: "#f5f5f5" }],
+        },
+        {
+          featureType: "administrative.land_parcel",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#bdbdbd" }],
+        },
+        {
+          featureType: "poi",
+          elementType: "geometry",
+          stylers: [{ color: "#eeeeee" }],
+        },
+        {
+          featureType: "poi",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#757575" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "geometry",
+          stylers: [{ color: "#e5e5e5" }],
+        },
+        {
+          featureType: "poi.park",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#9e9e9e" }],
+        },
+        {
+          featureType: "road",
+          elementType: "geometry",
+          stylers: [{ color: "#81C3D7" }],
+        },
+        {
+          featureType: "road.arterial",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#3A7CA5" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "geometry",
+          stylers: [{ color: "#2F6690" }],
+        },
+        {
+          featureType: "road.highway",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#2F6690" }],
+        },
+        {
+          featureType: "road.local",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#9e9e9e" }],
+        },
+        {
+          featureType: "transit.line",
+          elementType: "geometry",
+          stylers: [{ color: "#e5e5e5" }],
+        },
+        {
+          featureType: "transit.station",
+          elementType: "geometry",
+          stylers: [{ color: "#eeeeee" }],
+        },
+        {
+          featureType: "water",
+          elementType: "geometry",
+          stylers: [{ color: "#c9c9c9" }],
+        },
+        {
+          featureType: "water",
+          elementType: "labels.text.fill",
+          stylers: [{ color: "#9e9e9e" }],
+        },
+      ],
     });
 }
 
@@ -92,4 +186,5 @@ function changeLocation(latValue, lngValue) {
     myLatLng = new google.maps.LatLng({lat: latValue, lng: lngValue});
     new google.maps.Marker({position: {lat: latValue, lng: lngValue}, map: map}); 
     map.panTo(myLatLng);
+    map.setZoom(15);
 }
